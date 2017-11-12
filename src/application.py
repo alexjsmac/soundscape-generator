be6c8ae760948fb03227dcc06083dabb37411c95
Alex MacLean <amaclean@evertz.com>
2017-11-12 15:44:57 -0500

    Connect to frontend and add api call to return photo features

--- src/application.py
+++ src/application.py
@@ -1,18 +1,53 @@
 #!flask/bin/python
-from flask import Flask
+import os
+import boto3
+import botocore
+import requests
+
+from flask import Flask, render_template, make_response
 from flaskrun import flaskrun
+from flask_restful import Resource, Api, reqparse
 
-application = Flask(__name__)
+DIR = os.path.dirname(os.path.abspath(__file__))
+application = Flask(__name__,  static_folder='build/static')
+api = Api(application)
+
+s3 = boto3.client('s3')
+BUCKET = 'soundscape-generator-photos'
 
 
-@application.route('/', methods=['GET'])
-def get():
-    return '{"Output":"Hello World"}'
+def detect_labels(bucket, key, max_labels=10, min_confidence=80, region="us-east-1"):
+    rekognition = boto3.client("rekognition", region)
+    response = rekognition.detect_labels(
+        Image={
+            "S3Object": {
+                "Bucket": bucket,
+                "Name": key,
+            }
+        },
+        MaxLabels=max_labels,
+        MinConfidence=min_confidence,
+    )
+    return response['Labels']
+
+
+class Upload(Resource):
+    def get(self):
+        return detect_labels(BUCKET, 'lion-cubs.jpg')
+
+
+@application.route('/')
+def show_index():
+    return make_response(open(os.path.join(DIR, 'build/index.html')).read())
 
 
 @application.route('/', methods=['POST'])
 def post():
     return '{"Output":"Hello World"}'
 
+
+api.add_resource(Upload, '/api/v1/upload')
+
 if __name__ == '__main__':
-    flaskrun(application)
+    # flaskrun(application)
+    application.run(debug=True)
