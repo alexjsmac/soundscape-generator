de8e18bfd6919fff34b2ddce595d4f72af792f9f
gregalexsmith <gregalexsmith@gmail.com>
2018-06-27 23:01:19 -0400

    move combine reducers up a level

---
+++ src/frontend/src/core/reducers.js
@@ -0,0 +1,16 @@
+import {combineReducers} from 'redux';
+import { mediaReducer } from './media';
+import { audioReducer } from './audio';
+import { generalReducer } from './general';
+import { soundsReducer } from './sounds';
+import { appReducer } from './app';
+
+const rootReducer = combineReducers({
+  media: mediaReducer,
+  audio: audioReducer,
+  general: generalReducer,
+  sounds: soundsReducer,
+  app: appReducer
+});
+
+export default rootReducer;
