58851cb598c41042c61eb4bda018ce1686632729
greg <gregalexsmith@gmail.com>
2023-11-01 00:10:41 -0400

    cleanup

--- src/frontend/src/core/store/configureStore.js
+++ src/frontend/src/core/store/configureStore.js
@@ -1,13 +1,3 @@
-// async function getStore() {
-//   let module;
-//   if (import.meta.env.PROD === true) {
-//     module = await import('./configureStore.prod');
-//   } else {
-//     module = await import('./configureStore.dev');
-//   }
-//   return module.default;
-// }
-
-import getStore from "./configureStore.dev";
+import getStore from "./configureStore.prod";
 
 export default getStore;
