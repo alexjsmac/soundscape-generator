632304ecc923bde8781d8c4771b9e8b5852be1ed
greg <gregalexsmith@gmail.com>
2023-10-31 22:01:38 -0400

    add tailwind

--- src/frontend/src/index.jsx
+++ src/frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/index.css';
+import './index.css';
 
 const rootEl = document.getElementById('root')
 ReactDOM.render(<App />, rootEl);
