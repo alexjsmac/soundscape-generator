fb54e753cbebf3d21b173fb6f0059bfcc19c7e56
greg <gregalexsmith@gmail.com>
2023-11-13 22:22:50 -0500

    refactor to funcitonal component

--- client/src/components/media/MediaUploader.jsx
+++ client/src/components/media/MediaUploader.jsx
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { mediaActions } from "../../core/media";
-
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { Upload } from "antd";
 import styled from "styled-components";
+import { mediaActions } from "../../core/media";
 import { Card, H2 } from "../lib";
 
 const Dragger = Upload.Dragger;
@@ -12,57 +11,51 @@ const MediaUploadContainer = styled(Card)`
   margin: 1rem 0;
 `;
 
-class MediaUploader extends Component {
-  // When a user chooses a new media source
-  handleImageChange(file) {
-    const { setMedia } = this.props;
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setMedia({
-        source: reader.result,
-        fileName: file.name,
-      });
-    };
-    reader.readAsDataURL(file);
-    // Begin upload process
-    this.props.uploadMedia(file);
-  }
+const MediaUploader = () => {
+  const dispatch = useDispatch();
 
-  render() {
-    const uploaderProps = {
-      beforeUpload: (file) => {
-        this.handleImageChange(file);
-        return false;
-      },
-      showUploadList: false,
-    };
+  const handleImageChange = useCallback(
+    (file) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        dispatch(
+          mediaActions.setMedia({
+            source: reader.result,
+            fileName: file.name,
+          })
+        );
+      };
+      reader.readAsDataURL(file);
+      dispatch(mediaActions.uploadMedia(file));
+    },
+    [dispatch]
+  );
 
-    return (
-      <MediaUploadContainer>
-        <Dragger {...uploaderProps}>
-          <div className="grid grid-cols-12 gap-4">
-            <div className="col-span-8 p-2 text-left">
-              <H2>Your Soundscape</H2>
-              <p>
-                Upload your photo or video here to generate an audio soundscape.
-              </p>
-            </div>
-            <div className="col-span-3 flex items-center">
-              {/* <FileIcon /> */}
-              <span className="text-center font-bold">Upload Media Here</span>
-            </div>
-          </div>
-        </Dragger>
-      </MediaUploadContainer>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    source: state.media.source,
-    type: state.media.type,
+  const uploaderProps = {
+    beforeUpload: (file) => {
+      handleImageChange(file);
+      return false;
+    },
+    showUploadList: false,
   };
-}
 
-export default connect(mapStateToProps, mediaActions)(MediaUploader);
+  return (
+    <MediaUploadContainer>
+      <Dragger {...uploaderProps}>
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-8 p-2 text-left">
+            <H2>Your Soundscape</H2>
+            <p>
+              Upload your photo or video here to generate an audio soundscape.
+            </p>
+          </div>
+          <div className="col-span-3 flex items-center">
+            <span className="text-center font-bold">Upload Media Here</span>
+          </div>
+        </div>
+      </Dragger>
+    </MediaUploadContainer>
+  );
+};
+
+export default MediaUploader;
